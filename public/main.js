c6d6a7195249e01d76d57399e47843ac5bc1e195
unknown <aakashsm507@gmail.com>
2024-07-16 20:04:38 +0545

    Updated video chat implementation

--- public/main.js
+++ public/main.js
@@ -19,65 +19,54 @@ async function startVideoChat() {
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         weVideo.srcObject = localStream;
 
-        socket.emit('new-user');
-
-        socket.on('user-disconnected', () => {
-            strangerVideo.srcObject = null;
-            if (peerConnection) peerConnection.close();
+        socket.on('waiting', () => {
+            console.log('Waiting for a partner...');
         });
 
-        socket.on('offer', async (data) => {
+        socket.on('partner-found', async ({ partnerId }) => {
+            console.log('Partner found:', partnerId);
+
             peerConnection = new RTCPeerConnection(config);
             localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
             peerConnection.ontrack = event => {
                 strangerVideo.srcObject = event.streams[0];
             };
+
             peerConnection.onicecandidate = event => {
                 if (event.candidate) {
                     socket.emit('ice-candidate', { candidate: event.candidate });
                 }
             };
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
+
+            // Create an offer if this user is initiating the connection
+            if (socket.id < partnerId) {
+                const offer = await peerConnection.createOffer();
+                await peerConnection.setLocalDescription(offer);
+                socket.emit('offer', { offer });
+            }
+        });
+
+        socket.on('offer', async ({ offer }) => {
+            await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
             const answer = await peerConnection.createAnswer();
             await peerConnection.setLocalDescription(answer);
             socket.emit('answer', { answer });
         });
 
-        socket.on('answer', async (data) => {
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+        socket.on('answer', async ({ answer }) => {
+            await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
         });
 
-        socket.on('ice-candidate', async (data) => {
-            if (data.candidate) {
-                await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+        socket.on('ice-candidate', async ({ candidate }) => {
+            if (candidate) {
+                await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
             }
         });
 
-        socket.emit('ready');
-
-        socket.on('ready', async () => {
-            peerConnection = new RTCPeerConnection(config);
-            localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
-            peerConnection.ontrack = event => {
-                strangerVideo.srcObject = event.streams[0];
-            };
-            peerConnection.onicecandidate = event => {
-                if (event.candidate) {
-                    socket.emit('ice-candidate', { candidate: event.candidate });
-                }
-            };
-            const offer = await peerConnection.createOffer();
-            await peerConnection.setLocalDescription(offer);
-            socket.emit('offer', { offer });
-        });
-    } catch (err) {
-        console.error('Error accessing media devices.', err);
+    } catch (error) {
+        console.error('Error accessing media devices.', error);
     }
 }
-
-stopButton.addEventListener('click', () => {
-    if (peerConnection) peerConnection.close();
-    socket.disconnect();
-});
 
 startVideoChat();
--- server.js
+++ server.js
@@ -15,28 +15,42 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let waitingSocket = null;
+
 // Handle socket connection
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
-    // Broadcast a message when a user disconnects
+    if (waitingSocket) {
+        // If there is a socket waiting, pair them
+        const partnerSocket = waitingSocket;
+        waitingSocket = null;
+
+        // Notify both users that they are connected
+        socket.emit('partner-found', { partnerId: partnerSocket.id });
+        partnerSocket.emit('partner-found', { partnerId: socket.id });
+
+        // Set up event listeners for signaling data
+        socket.on('offer', (data) => partnerSocket.emit('offer', data));
+        socket.on('answer', (data) => partnerSocket.emit('answer', data));
+        socket.on('ice-candidate', (data) => partnerSocket.emit('ice-candidate', data));
+
+        partnerSocket.on('offer', (data) => socket.emit('offer', data));
+        partnerSocket.on('answer', (data) => socket.emit('answer', data));
+        partnerSocket.on('ice-candidate', (data) => socket.emit('ice-candidate', data));
+
+    } else {
+        // If no one is waiting, set this socket as waiting
+        waitingSocket = socket;
+        socket.emit('waiting');
+    }
+
+    // Handle disconnection
     socket.on('disconnect', () => {
         console.log('A user disconnected:', socket.id);
-        socket.broadcast.emit('user-disconnected', socket.id);
-    });
-
-    // Relay offer and answer between peers
-    socket.on('offer', (data) => {
-        socket.broadcast.emit('offer', data);
-    });
-
-    socket.on('answer', (data) => {
-        socket.broadcast.emit('answer', data);
-    });
-
-    // Relay ICE candidates between peers
-    socket.on('ice-candidate', (data) => {
-        socket.broadcast.emit('ice-candidate', data);
+        if (waitingSocket === socket) {
+            waitingSocket = null;
+        }
     });
 });
 
